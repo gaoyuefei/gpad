1c3bf11b5518fb671b7cc5636ebbf571c079bfd1
LFLibra <837114501@qq.com>
2023-10-27 17:50:15 +0800

    提交10-27 打印日志

--- gpad-modules/gpad-tool/src/main/java/com/gpad/gpadtool/service/GRTService.java
+++ gpad-modules/gpad-tool/src/main/java/com/gpad/gpadtool/service/GRTService.java
@@ -278,9 +278,10 @@ public class GRTService {
         HttpEntity<String> requestEntity = new HttpEntity<>( httpHeaders);
 
         String url = String.format(orderDetailUrl,bussinessNo);
+        log.info("查询交车确认信息 --->>> {}", JSONObject.toJSONString(url));
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
         OrderDetailListResultDto orderDetailListResultDto = JSONObject.parseObject(response.getBody(), OrderDetailListResultDto.class);
-
+        log.info("查询交车确认信息 --->>> {}", JSONObject.toJSONString(response));
         if (orderDetailListResultDto == null || orderDetailListResultDto.getStatus() == null){
             return R.fail("对接GRT获取待交车订单详情出错!接口返回null  ");
         }else if (!"200".equals(orderDetailListResultDto.getStatus())){
