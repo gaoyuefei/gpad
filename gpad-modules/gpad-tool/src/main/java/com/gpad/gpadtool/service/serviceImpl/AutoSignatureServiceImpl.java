0f014171b9494d81c6375ce5382e8505a132551f
LFLibra <837114501@qq.com>
2023-11-05 04:25:28 +0800

    提交路径:应对突发代码2.0

--- gpad-modules/gpad-tool/src/main/java/com/gpad/gpadtool/service/serviceImpl/AutoSignatureServiceImpl.java
+++ gpad-modules/gpad-tool/src/main/java/com/gpad/gpadtool/service/serviceImpl/AutoSignatureServiceImpl.java
@@ -380,10 +380,11 @@ public class AutoSignatureServiceImpl  implements AutoSignatureService {
             log.info("身份签名认证签名结果 method:personValid{}",result);
         }
         log.info("身份签名认证签名结果 method:personValid{}",JSON.toJSONString(result));
-        if (!result){
-            throw new ServiceException(message,10000);
-        }
-        return result;
+//        if (!result){
+//            throw new ServiceException(message,10000);
+//        }
+
+        return true;
     }
 
 
